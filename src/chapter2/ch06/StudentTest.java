52ad2347de33df95268ac6c38d8c183b48abce21
Irang <irang0426@gmail.com>
2022-10-31 13:53:59 +0900

    생ì 성자 테스트

---
+++ src/chapter2/ch06/StudentTest.java
@@ -0,0 +1,14 @@
+package chapter2.ch06;
+
+public class StudentTest {
+    public static void main(String[] args) {
+
+        Student studentKim = new Student();
+
+        System.out.println(studentKim.showStudentInfo());
+
+        Student studentLee = new Student(123456, "Lee", 3);
+
+        System.out.println(studentLee.showStudentInfo());
+    }
+}
