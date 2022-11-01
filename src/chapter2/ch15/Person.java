aab71ace5f763e4db5b25117f53182b549f473c6
Irang <irang0426@gmail.com>
2022-11-01 16:02:55 +0900

    객체 협력 ë복습

---
+++ src/chapter2/ch15/Person.java
@@ -0,0 +1,20 @@
+package chapter2.ch15;
+
+public class Person {
+    String name;
+    int money;
+
+    public Person(String name, int money) {
+        this.name = name;
+        this.money = money;
+    }
+
+    public void takeTaxi(Taxi taxi, int money) {
+        taxi.take(money);
+        this.money -= money;
+    }
+
+    public void showPersonInfo() {
+        System.out.println(name + "님의 남은 돈은 " + money + "원 입니다.");
+    }
+}
---
+++ src/chapter2/ch15/Test.java
@@ -0,0 +1,13 @@
+package chapter2.ch15;
+
+public class Test {
+    public static void main(String[] args) {
+        Person person = new Person("Edward", 20000);
+        Taxi taxi = new Taxi("잘 간다 운수택시");
+
+        person.takeTaxi(taxi, 10000);
+
+        person.showPersonInfo();
+        taxi.showTaxiInfo();
+    }
+}
