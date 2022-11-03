8aec226cd86a652aebcf632787fc8db97ee38ab1
Irang <irang0426@gmail.com>
2022-11-03 15:35:06 +0900

    ch06 다형성과 다형성을 사용하는 이유

---
+++ src/irang/chapter3/ch06/VIPCustomer.java
@@ -0,0 +1,37 @@
+package irang.chapter3.ch06;
+
+public class VIPCustomer extends Customer {
+    double salesRatio;
+    private String agentID;
+
+    /*public VIPCustomer() {
+        customerGrade = "VIP";
+        bonusRatio = 0.05;
+        salesRatio = 0.1;
+
+        System.out.println("VIPCustomer() call");
+    }*/
+
+    public VIPCustomer(int customerID, String customerName) {
+        super(customerID, customerName);
+
+        customerGrade = "VIP";
+        bonusRatio = 0.05;
+        salesRatio = 0.1;
+    }
+
+    public String getAgentID() {
+        return agentID;
+    }
+
+    public void setAgentID(String agentID) {
+        this.agentID = agentID;
+    }
+
+    @Override
+    public int calcPrice(int price) {
+        bonusPoint += price * bonusRatio;
+        price -= (int)(price * salesRatio);
+        return price;
+    }
+}
