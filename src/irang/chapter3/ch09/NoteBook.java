9a6e923062603b37d3c2e16530a82f9232e4d705
Irang <irang0426@gmail.com>
2022-11-04 13:54:21 +0900

    ch09 추상 클래스의 의미와 구현하는 방법

---
+++ src/irang/chapter3/ch09/NoteBook.java
@@ -0,0 +1,8 @@
+package irang.chapter3.ch09;
+
+public abstract class NoteBook extends Computer{
+    @Override
+    public void display() {
+        System.out.println("NoteBook display");
+    }
+}
