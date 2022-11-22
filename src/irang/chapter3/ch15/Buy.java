576a5893a2e325e9bc9515a9a53ffca33915131d
Irang <irang0426@gmail.com>
2022-11-22 14:27:44 +0900

    ch15 여러 인터페이스 구현하기, ì인터페이스의 상속

---
+++ src/irang/chapter3/ch15/Buy.java
@@ -0,0 +1,9 @@
+package irang.chapter3.ch15;
+
+public interface Buy {
+    void buy();
+
+    default void order() {
+        System.out.println("buy order");
+    }
+}
---
+++ src/irang/chapter3/ch15/CustomerTest.java
@@ -0,0 +1,20 @@
+package irang.chapter3.ch15;
+
+public class CustomerTest {
+    public static void main(String[] args) {
+
+        Customer customer = new Customer();
+        customer.buy();
+        customer.sell();
+        customer.order();
+        customer.hello();
+
+        Buy buyer = customer;
+        buyer.buy();
+        buyer.order();
+
+        Sell seller = customer;
+        seller.sell();
+        seller.order();
+    }
+}
